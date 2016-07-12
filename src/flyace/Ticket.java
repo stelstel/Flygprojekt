30bc219d058bea91ab839dfc3958402b40a44935
amirdolkhani <amirdolkhani@10.218.52.233>
2016-07-12 14:23:20 +0200

    Third Commit

--- src/flyace/Ticket.java
+++ src/flyace/Ticket.java
@@ -1,21 +1,16 @@
-
 package flyace;
 
 import java.util.Date;
 
 public class Ticket {
-    
-    
-    
-     
+
     private Company company;
-    private Customer customer ;
+    private Customer customer;
     private AirPlane plane;
     private Seat seat;
-    private double price ;
+    private double price;
     private Date expired;
-    
-
+    private SeatClass seatclass;
 
     public Company getCompany() {
         return company;
@@ -24,47 +19,39 @@ public class Ticket {
     public void setCompany(Company company) {
         this.company = company;
     }
-    
+
     public Customer getCustomer() {
         return customer;
     }
 
-    
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
 
-    
     public AirPlane getPlane() {
         return plane;
     }
 
-    
     public void setPlane(AirPlane plane) {
         this.plane = plane;
     }
 
-   
     public Seat getSeat() {
         return seat;
     }
 
-   
     public void setSeat(Seat seat) {
         this.seat = seat;
     }
 
-    
     public double getPrice() {
         return price;
     }
 
-    
     public void setPrice(double price) {
         this.price = price;
     }
 
-    
     public Date getExpired() {
         return expired;
     }
@@ -73,4 +60,11 @@ public class Ticket {
         this.expired = expired;
     }
 
+    public SeatClass getSeatclass() {
+        return seatclass;
+    }
+
+    public void setSeatclass(SeatClass seatclass) {
+        this.seatclass = seatclass;
+    }
 }
