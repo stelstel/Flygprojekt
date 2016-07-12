929977a1866d7bad1e6904607a307a1e88168387
stelstel <sthlm_32@hotmail.com>
2016-07-12 09:55:20 +0200

    New Class, Seat

--- src/flyace/Seat.java
+++ src/flyace/Seat.java
@@ -8,6 +8,15 @@ package flyace;
  *
  * @author User
  */
+
+//Seat					Stefan
+	//seatClass = First or Economy [Titta på var customer sitter i Passangerplane, 0-5 first class, 6-10 economy class]
+	//seatNumber = 1-10 
+	//status = idle or Customer object [om indexet tomt, ingen customer]
+	//price = 20000kr or 5000kr [se ovan First or economy]
+	//assignCustomer() [putCustomer i PassengerPlane]
+	//removeCustomer() [freeSeat i PassengerPlane]
+
 public class Seat {
     
 }
