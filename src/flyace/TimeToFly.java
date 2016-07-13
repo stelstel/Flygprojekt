5bd9d8c3fd2ee3c5e76f4e109858b6c363fb776a
Stefan <Stefan@192.169.0.194>
2016-07-13 21:06:32 +0200

    UML

---
+++ src/flyace/TimeToFly.java
@@ -0,0 +1,11 @@
+package flyace;
+
+/**
+ *
+ * @author Stefan Elmgren
+ * @version 1.00
+ * Checks if all seats are occupied. If thats the case starts the flight
+ */
+public final class TimeToFly {
+    
+}
