e2c1c6aad9e50eae9f1b194bb10b346f487e55e8
amirdolkhani <amirdolkhani@10.218.52.233>
2016-07-13 15:42:01 +0200

    Fifth Commit

--- src/flyace/Money.java
+++ src/flyace/Money.java
@@ -1,28 +1,32 @@
-
+//
+////import flyace.SeatClass;
 //import flyace.SeatClass;
-import flyace.Ticket;
-import food.FoodOrder;
-
-
-public class Money {
-
-    private double saldo;
-    private double income;
-    private double cost;
-   // Ticket ticketPrice = new Ticket();
-   // FoodOrder food = new FoodOrder(SeatClass seatClass);  
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    
-    public double calcSuldo() {
-       return  food.getTotalCost() + ticketPrice.getPrice();
-    }
-}
-
-//	saldo	
-//	income()
+//import flyace.Ticket;
+//import food.FoodOrder;
+//
+//public class Money {
+//
+//    private double saldo;
+//    private double income;
+//    private double cost;
+//    
+//    
+//    Ticket ticketPrice = new Ticket();
+//    
+//    FoodOrder foodFirst = new FoodOrder(SeatClass.FIRST);
+//    FoodOrder foodEconomy = new FoodOrder(SeatClass.ECONOMY);
+//    
+//    private double price = ticket.getSeat().getPrice();
+//    public double getSaldo() {
+//        return saldo;
+//    }
+//
+//    public void setSaldo(double saldo) {
+//        this.saldo = saldo;
+//    }
+//
+//    public double calcSuldo() {
+//        return food.getTotalCost() + ticketPrice.getPrice();
+//    }
+//}
+//
