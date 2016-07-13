572bf768689a59ba0d3f1c78dcc3510ec8d666f0
User <User@JJZ7J32>
2016-07-13 12:44:03 +0200

    Test av Commit

--- src/flyace/BookingSystem.java
+++ src/flyace/BookingSystem.java
@@ -1,77 +1,129 @@
 package flyace;
 
 //Test Commit
-
-import java.util.ArrayList;
-
+import java.util.HashMap;
 
 public class BookingSystem {
-	private ArrayList<Customer> customers = new ArrayList<Customer>();
-	private Company company;
-        private Ticket ticket;
-        private PassengerPlane plane;
-        private int uniqueCustomerId;
-        
-	public BookingSystem(){
-		this.company = new Company();
-                uniqueCustomerId = 1;
-		// Buy one initial plane to the company
-		plane = new PassengerPlane("Boeing 747", 10);
-		company.buyPlane(plane);
-	}
-	
-//	public Customer addCustomer(String firstName, String lastName, SeatClass seatClass){
-	public Ticket addCustomer(String firstName, String lastName, SeatClass seatClass){
-		// Find a plane
-		ArrayList<PassengerPlane> planes = company.getPlanes();
-		if(planes.size() == 0){
-			return null;
-		}
-		plane = planes.get(0);
+    private HashMap<Integer, Ticket> tickets = new HashMap<Integer, Ticket>();
 
-		//Reserv a seat for the customer
-		Customer customer = new Customer(firstName, lastName, String.valueOf(uniqueCustomerId));
-      		//TODO: Use Ticket 
-                ticket = new Ticket();
-                ticket.setCustomer(customer);
-                ticket.setSeatClass(seatClass);
-                company.putCustomer(ticket);
-                if(ticket.getSeat() == null){
-System.out.println("BookingSystem: No Seat found in ticket");
-                }
-                else{
-System.out.println("BookingSystem: Yes a Seat found in ticket");
-                }
-                //instead of this
-//                plane.putCustomer(customer, seatClass);
+    private Company company;
+    private Ticket ticket;
+    private PassengerPlane plane;
+    private int uniqueCustomerId;
+
+    public BookingSystem() {
+        this.company = new Company();
+        uniqueCustomerId = 1;
+        // Buy one initial plane to the company
+        plane = new PassengerPlane("Boeing 747", 10);
+        company.buyPlane(plane);
+    }
+
+    public Ticket addCustomer(String firstName, String lastName, SeatClass seatClass) {
+        //Reserv a seat for the customer
+        Customer customer = new Customer(firstName, lastName, String.valueOf(uniqueCustomerId));
+        ticket = new Ticket();
+        ticket.setCustomer(customer);
+        ticket.setSeatClass(seatClass);
+        company.putCustomer(ticket);
+        //Check if the reservation was successful, if a Seat was reserved then the ticket is valid
+        if(ticket.getSeat() != null){
+            // If valid ticket, then add it to the tickets HashMap
+            tickets.put(uniqueCustomerId, ticket);
+            uniqueCustomerId++;
+        }
+        return ticket;
+    }
+
+    public Ticket findCustomerbyNumber(int customerNumber) {
+        ticket = tickets.get(customerNumber);
+        return ticket;
+    }
+
+    public String showTicketInformation(Ticket t) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getTicketHeader());
+        sb.append(getTicketContent(t));
+        sb.append("\n");
+        return sb.toString();
+    }
 
 
-		customers.add(customer);
-                uniqueCustomerId++;
-//		return customer;
-		return ticket;
-	}
-       	public String showAllCustomers(){
-                String header =  "Number  Class   Fist name           Last name\n";
-                String header2 = "------  -----   ---------           ---------\n";
-                String header3 = "                                               ";
+    public String showTicketInformation2(Ticket t) {
+        Customer c = t.getCustomer();
+        String header =  "Num. Fist name           Last name           Airplane            Seat Class     Price     Status\n";
+        String header2 = "---- ---------           ---------           --------            ---- -----     -----     ------\n";
+        StringBuilder sb = new StringBuilder(header);
+        sb.append(header2);
+        sb.append(fixLengthString(c.getPersonalNumber(),5));
+        sb.append(fixLengthString(c.getFirstName(),20));
+        sb.append(fixLengthString(c.getsurname(),20));
+        sb.append(fixLengthString(t.getPlane().getName(),20));
+        sb.append(fixLengthString(String.valueOf(t.getSeat().getSeatNumber()),5));
+        if(t.getSeat().getSeatclass() == SeatClass.FIRST)
+            sb.append(fixLengthString("First",10));
+        else
+            sb.append(fixLengthString("Economy",10));
+        sb.append(fixLengthString(String.valueOf(t.getSeat().getPrice()),10));
+        if(t.getSeat().getSeatstatus() == SeatStatus.FREE)
+            sb.append(fixLengthString("Free",10));
+        else
+            sb.append(fixLengthString("Occupied",10));
+        //TODO: Add if FoodOrderExist or not
+        sb.append("\n");
+        return sb.toString();
+    }
 
-       		StringBuilder sb = new StringBuilder(header);
-                sb.append(header2);
-                StringBuilder sb2 = new StringBuilder(header3);
-                
-		for(Customer c : customers){
-                    sb2.insert(0, c.getPersonalNumber());
-//                    sb2.insert(8, c.getClass());
-                    sb2.insert(16, c.getFirstName());
-                    sb2.insert(36, c.getsurname() +"\n");
-                    sb.append(sb2.toString());
-                    sb2.setLength(0);
-                    sb2.append(header);
-                }
-		return sb.toString();
-	}
+    public String showAllCustomers() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getTicketHeader());
+        for(int i=1; i-1 < tickets.size(); i++){
+            ticket = tickets.get(i);
+            sb.append(getTicketContent(ticket));
+        }
+        return sb.toString();
+    }
+
+    private StringBuilder getTicketHeader(){
+        String header =  "Num. Fist name           Last name           Airplane            Seat Class     Price     Status\n";
+        String header2 = "---- ---------           ---------           --------            ---- -----     -----     ------\n";
+        StringBuilder sb = new StringBuilder(header);
+        sb.append(header2);
+        return sb;
+    }
+
+    private StringBuilder getTicketContent(Ticket t){
+        Customer c = t.getCustomer();
+        StringBuilder sb = new StringBuilder();
+        sb.append(fixLengthString(c.getPersonalNumber(),5));
+        sb.append(fixLengthString(c.getFirstName(),20));
+        sb.append(fixLengthString(c.getsurname(),20));
+        sb.append(fixLengthString(t.getPlane().getName(),20));
+        sb.append(fixLengthString(String.valueOf(t.getSeat().getSeatNumber()),5));
+        if(t.getSeat().getSeatclass() == SeatClass.FIRST)
+            sb.append(fixLengthString("First",10));
+        else
+            sb.append(fixLengthString("Economy",10));
+        sb.append(fixLengthString(String.valueOf(t.getSeat().getPrice()),10));
+        if(t.getSeat().getSeatstatus() == SeatStatus.FREE)
+            sb.append(fixLengthString("Free",10));
+        else
+            sb.append(fixLengthString("Occupied",10));
+        //TODO: Add if FoodOrderExist or not
+        sb.append("\n");
+        return sb;
+    }
+
+
+    private String fixLengthString(String start, int length) {
+        if (start.length() >= length) {
+            return start.substring(0, length);
+        } else {
+            while (start.length() < length) {
+                start += " ";
+            }
+            return start;
+        }
+    }
 
 }
-
-
