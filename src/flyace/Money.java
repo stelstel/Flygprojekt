831abd5421e486062cf6fdced6d122af2abf58bc
amirdolkhani <amirdolkhani@amirs-mbp.lan>
2016-07-14 06:55:55 +0200

    Money update

--- src/flyace/Money.java
+++ src/flyace/Money.java
@@ -1,36 +1,37 @@
 package flyace;
 
-import flyace.SeatClass;
-import flyace.Ticket;
 import food.FoodOrder;
 
 public class Money {
 
     private double saldo;
     private double income;
-    private double cost;
+    private double profit;
     
     
-    Ticket ticketPrice = new Ticket();
+    Ticket ticket;
+    SeatClass seatClass;
+    FoodOrder foodOrder;
     
-    FoodOrder foodFirst = new FoodOrder(SeatClass.FIRST);
-    FoodOrder foodEconomy = new FoodOrder(SeatClass.ECONOMY);
-    
-    // TODO commented out temporarly by Stefan
-    // private double price = ticket.getSeat().getPrice();
     public double getSaldo() {
+        saldo = foodOrder.getTotalCost() + ticket.getPrice();
         return saldo;
     }
-
+    
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
     
+    public double getIncome() {
+        return income;
+    }
     
-
-    // TODO commented out temporarly by Stefan
-//    public double calcSuldo() {
-//        return food.getTotalCost() + ticketPrice.getPrice();
-//    }
+    public void setIncome(double income) {
+        this.income = income;
+    }
+    
+    public double getProfit(double profit) {
+        profit = 0.3 * income;
+        return profit;
+    }
 }
-
